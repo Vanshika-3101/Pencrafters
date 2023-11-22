c3184b36a0a362f97f510330498c6c01a56d0048
Vanshika-3101 <114131049+Vanshika-3101@users.noreply.github.com>
2023-11-23 00:05:21 +0530

    Add files via upload

---
+++ code/public/js/firebase.js
@@ -0,0 +1,11 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyDY31FRMXDvb5SvX4J3qrPR4eSvINuH0FM",
+    authDomain: "blogging-website-adef4.firebaseapp.com",
+    projectId: "blogging-website-adef4",
+    storageBucket: "blogging-website-adef4.appspot.com",
+    messagingSenderId: "920096651072",
+    appId: "1:920096651072:web:041ea4a8d0bf7ac2196ca5"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
